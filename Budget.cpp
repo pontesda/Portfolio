eb0dc7b3a776b9a56e77a2734b67f955420c41f4
Dillon Pontes <55772248+pontesda@users.noreply.github.com>
2019-10-01 23:30:07 -0400

    Update Budget.cpp
    This program writes to a file named “Budget.txt” that asks the user to enter the amount that he or she has budgeted for a month.  
    A loop then prompts the user to enter each of his or her expenses for the month and keep a running total.  
    The program writes both the expense name and corresponding expense, along with how much he or she is over-budget or under-budget to the file.
    EXAMPLE:
    Enter the amount for budgeting: 1200
    Please enter the name of your expense followed by the amount.
    Enter ‘#’ to quit.
    Enter an expense: Gas 15.50
    Enter an expense: Food 250.00
    Enter an expense: #
    Data has been written to the file.

--- Budget.cpp
+++ Budget.cpp
@@ -1,17 +1,3 @@
-/*
-2.	
-Write a program to a file named “Budget.txt” that asks the user to enter the amount that he or she has budgeted for a month.  
-A loop should then prompt the user to enter each of his or her expenses for the month and keep a running total.  
-The program should write both the expense name and corresponding expense, along with how much he or she is over-budget or under-budget to the file.
-EXAMPLE:
-Enter the amount for budgeting: 1200
-Please enter the name of your expense followed by the amount.
-Enter ‘#’ to quit.
-Enter an expense: Gas 15.50
-Enter an expense: Food 250.00
-Enter an expense: #
-Data has been written to the file.
-*/
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -19,40 +5,45 @@ using namespace std;
 int main()
 {
 	ofstream outputFile;
-	int budget, total = 0, amount = 1;
-	const string sentinal = "#";
+	int budget, total = 0, amount;
 	string expenseName;
 	outputFile.open("Budget.txt");
 
 		// Get the budget and expenses from the user.
 		cout << "Enter your budget" << endl;
 		cin >> budget;
-		// Get the amount of expenses.
+		outputFile << "Your budget is $" << budget << "." << endl;
+		// Get the amount of expenses via while loop.
 		while (expenseName != "#")
 		{
 			cout << "Enter your expenses followed by the amount." << endl;
 			cout << "Enter '#' to quit." << endl;
 			cin >> expenseName;
+			//To end the loop if the user decides to quit.
 			if (expenseName == "#")
 				break;
+			//If user chooses not to quit then the program will process the amount and write the expense to the text file.
 			else
 			{
 				cin >> amount;
-				outputFile << expenseName << " " << amount << endl;
+				outputFile << expenseName << " $" << amount << endl;
 				total += amount;
 			}
-			if (total < budget)
-			{
-				cout << "You are under budget." << endl;
-				outputFile << "You are under budget." << endl;
-			}
-			else
-			{
-				cout << "You are over budget." << endl;
-				outputFile << "You are over budget." << endl;
-			}
 		}
+		//If the user decides to quit, then the program will determine if the user is over/under budget by comparing the total expenses to the budget.
+		if (total < budget)
+		{
+			cout << "You are under budget." << endl;
+			outputFile << "You are under budget." << endl;
+		}
+		else
+		{
+			cout << "You are over budget." << endl;
+			outputFile << "You are over budget." << endl;
+		}
+		//Close the file.
 		outputFile.close();
+		//Infrom the user that the data is written.
 		cout << "Data has been written to the file.";
 		return 0;
 }
