a915c0526367c85457157af1e37b72e454029eee
Dillon Pontes <55772248+pontesda@users.noreply.github.com>
2020-02-21 14:28:11 -0500

    Create source.cpp

---
+++ carSimulator/source.cpp
@@ -0,0 +1,8 @@
+#include "FUELGAUGE.h"
+#include "ODOMETER.h"
+int main() {
+
+	odometer car1;			// Create a object
+	car1.simulateDriving(); // Activate object's main function.
+	return 0;				// End program.
+}
